c677d2f95aa0904218c22c7404e356b074f39e18
Joyce <jyu21@ncsu.edu>
2015-05-20 18:59:56 -0400

    edits made to tech talent south blog with user login

--- tech_talent_blog/app/controllers/blog_posts_controller.rb
+++ tech_talent_blog/app/controllers/blog_posts_controller.rb
@@ -1,6 +1,12 @@
 class BlogPostsController < ApplicationController
   before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
 
+  def your_posts
+  end
+
+  def user_posts
+    @user = User.find(params[:id])
+  end 
   # GET /blog_posts
   # GET /blog_posts.json
   def index
@@ -74,6 +80,6 @@ class BlogPostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_post_params
-      params.require(:blog_post).permit(:title, :author, :blog_entry)
+      params.require(:blog_post).permit(:title, :author, :blog_entry, :user_id)
     end
 end
--- tech_talent_blog/db/schema.rb
+++ tech_talent_blog/db/schema.rb
@@ -11,22 +11,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513150155) do
+ActiveRecord::Schema.define(version: 20150520190414) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.text     "blog_entry"
-    t.datetime "created_at" null: false 
-    t.datetime "updated_at" null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
     t.text     "comment_entry"
-    t.datetime "created_at"
-    t.datetime "updated_at" null: false
-    t.integer  "blog_post_id" null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "blog_post_id"
+    t.integer  "user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "username"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
